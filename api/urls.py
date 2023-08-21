55631f9b1e4c1261f293677ed4fe5a6b720bec14
Denzel Gitonga <gitongadenzel@gmail.com>
2023-08-21 13:54:14 +0300

    correct urls

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes), # get Json Response
-    path('/notes', views.getNotes), # get the notes
+    path('notes/', views.getNotes), # get the notes
 ]
