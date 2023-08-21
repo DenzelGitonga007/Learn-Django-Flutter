9ebefc54be23e80f60b5ebe08d1a6332f0dd3259
Denzel Gitonga <gitongadenzel@gmail.com>
2023-08-21 13:14:18 +0300

    urls and views

--- project_deheal/settings.py
+++ project_deheal/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'api', # handle backend
     'rest_framework', # api
 ]
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
